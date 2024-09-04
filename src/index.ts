1ae900d9078fa6640ccd412243ba123f39975fb8
Ethan McGee <ethan.mcgee@yubico.com>
2024-09-04 13:16:27 -0400

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,59 @@
  */
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(req, env, ctx): Promise<Response> {
+		if (req.method === "OPTIONS") {
+			return new Response('', {
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+					'Access-Control-Allow-Origin': req.headers.get("Origin")!,
+					'Access-Control-Allow-Methods': 'POST, OPTIONS',
+					'Access-Control-Allow-Headers': '*',
+				}
+			});
+		}
+
+		const RECAPTCHA_TOKEN = env.RECAPTCHA_TOKEN;
+		const MAILGUN_USER = env.MAILGUN_USER;
+		const MAILGUN_KEY = env.MAILGUN_KEY;
+		const MAILGUN_DOMAIN = env.MAILGUN_DOMAIN;
+		const SMTP_TO = env.SMTP_TO;
+
+		const body = await req.json();
+		
+		const data = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_TOKEN}&response=${body.token}`, {
+			method: 'POST'
+		}).then(res => res.json());
+
+		if (data.success) {
+			const form = new FormData();
+			form.append('from', body.email);
+			form.append('to', SMTP_TO);
+			form.append('subject', `Contact Form Submission: ${body.first_name} ${body.first_name}`);
+			form.append('text', body.message);
+
+			const resp = await fetch(
+				`https://api.mailgun.net/v3/${MAILGUN_DOMAIN}/messages`,
+				{
+					method: 'POST',
+					headers: {
+						Authorization: 'Basic ' + btoa(`${MAILGUN_USER}:${MAILGUN_KEY}`)
+					},
+					body: form
+				}
+			);
+			return new Response(JSON.stringify(resp), {
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+					'Access-Control-Allow-Origin': req.headers.get("Origin")!,
+				}
+			});
+		}
+		return new Response(JSON.stringify(data), {
+			headers: {
+				'content-type': 'application/json;charset=UTF-8',
+					'Access-Control-Allow-Origin': req.headers.get("Origin")!,
+			}
+		});
 	},
 } satisfies ExportedHandler<Env>;
